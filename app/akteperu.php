d2f6327813cff32bd5d87044f9fe48288f7983a2
Bayu <bayuamdan@gmail.com>
2020-08-10 03:34:50 +0000

    Upload New File

---
+++ app/akteperu.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class akteperu extends Model
+{
+    protected $fillable = [
+        'Akte_Pendirian_No','Bank','Rek','File_Akte','File_Pengesahan','Apt_No'
+        ,'Tgl_Apt','File_Apt','Tgl_Direksi','File_ktp','File_Spm','File_Sp'
+    ];
+}
